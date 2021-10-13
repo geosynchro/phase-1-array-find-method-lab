b0a3e6b5a0001be1adddb08105f43b474de20f16
Nicholas Fagner <nicholasfagner@gmail.com>
2021-10-13 17:35:36 -0600

    commit tree 600d962babb8473cbf7449d47d8beb1661dfdea3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function superbowlWin(array) {
+    for (let i = 0; i < array.length; i++) {
+        if(array.find(element => array[i].result === "W")) {
+         return array[i].year;
+        }
+    } 
+}
 
